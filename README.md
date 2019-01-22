0f47bdd109326f2dd8482c560805a0671078a1ee
Alexander Granhof <alexander.g737@gmail.com>
2019-01-22 21:07:31 +0100

    Added asynchronous mode for running the drone and updated README

--- README.md
+++ README.md
@@ -1,11 +1,100 @@
 # tello-drone
 
 ### Disclaimer:
+
 Im a student learning how to work and publish packages on NPM. This is a simple package that allows you to communicate with a DJI Tello drone in a much easier way. Any feedback / help / improvement is appreaciated and i will hopefully be able to look into fixing bugs and such when i can.
 
 This project is currently a work in progress, but as of 1.0.1 it can communicate with a tello drone with the sample code bellow.
 
-# Example
+## Information
+
+You can communicate with the drone synchronous or asynchronous. You can pass an option object to switch to asynchronous (synchronous by default). When you connect to the drone it **automatically sends the 'command' command**, so that is not nececary. If you are working with the drone synchronous, you need to wait for the OK message from the drone before you can send the next command.
+
+## Error handling
+
+The drone gives very minimal feedback on errors. Command verification is already built in, it will throw an error if a command is invalid.
+If you recieve back an error and the drone does not execute the command, the most common reason in my experience is low battery.
+Charging the battery and trying again solves that error issue.
+
+## Methods and options
+
+```js
+const tello = require("tello-drone");
+
+// All option parameters are optional
+const drone = tello.connect(
+    {
+      async: true,           // makes the send method async. default: false
+      host: "192.168.0.123", // manually set the host        default: "192.168.10.1"
+      port: "3333",          // manually set the port        default: "8889"
+      statePort: "3334",     // manually set the state port  default: "8890"
+      skipOK: true,          // dont send the OK message     default: false
+      bufferOffset: 10       // buffer offset for dgram      default: 0
+    }
+);
+
+// Sends a command to the drone
+drone.send("battery?")
+
+// Sends a command to the drone without verifying the command
+drone.forceSend("battery?")
+
+// Attaches a callback to a specific event
+drone.on(/* ... */)
+
+/** All events of the on method
+ * connection   - Fired when connected to the drone             Parameters: []
+ * state        - Fired when the drone sends its state message  Parameters: [stateObject, udpConnection]
+ * send         - Fired when a command is sent                  Parameters: [error, messageLength]
+ * message      - Fired when the drone sends a status message   Parameters: [message, udpConnection]
+ */
+
+
+tello.DELAYS // All the rough delays for each command when in synchronous mode
+
+```
+
+# Asynchronous example
+
+```js
+const tello = require("tello-drone");
+
+const drone = tello.connect({ async: true });
+
+drone.on("connection", () => {
+    console.log("Connected to drone");
+});
+
+drone.on("state", state => {
+    console.log("Recieved State > ", state);
+});
+
+drone.on("send", (err, length) => {
+    if (err) console.log(err);
+
+    console.log(`Sent command is ${length} long`);
+});
+
+drone.on("message", message => {
+    console.log("Recieved Message > ", message);
+});
+
+(async () => {
+    try {
+        await drone.send("takeoff");
+        await drone.send("flip", { value: "f" });
+        await drone.send("land");
+    } catch (error) {
+        throw new Error(error);
+    }
+})();
+```
+
+# Video demo
+
+[![Youtube video, broken :/](http://img.youtube.com/vi/pxh4rlVNd4E/0.jpg)](http://www.youtube.com/watch?v=pxh4rlVNd4E "NPM tello-drone v2.0.0 demo")
+
+# Synchronous example
 
 ```js
 const tello = require("tello-drone");
@@ -13,15 +102,11 @@ const tello = require("tello-drone");
 const drone = tello.connect();
 
 drone.on("connection", () => {
-    console.log("Listening");
+    console.log("Connected to drone");
 });
 
 drone.on("state", state => {
     console.log("Recieved State > ", state);
-});
-
-drone.on("error", err => {
-    console.log("Recieved Error > ", err);
 });
 
 drone.on("send", (err, length) => {
@@ -35,13 +120,19 @@ drone.on("message", message => {
 });
 
 drone.send("battery?");
-
 ```
 
 ## Avaliable commands to send
+
 Tellos official SDK documentation with all commands:
 
-https://dl-cdn.ryzerobotics.com/downloads/Tello/Tello%20SDK%202.0%20User%20Guide.pdf
+#### Check your firmware version for the right manual (most likely 1.3)
+
+1.0: https://dl-cdn.ryzerobotics.com/downloads/tello/0228/Tello+SDK+Readme.pdf
+
+1.3: https://dl-cdn.ryzerobotics.com/downloads/tello/20180910/Tello%20SDK%20Documentation%20EN_1.3.pdf
+
+2.0: https://dl-cdn.ryzerobotics.com/downloads/Tello/Tello%20SDK%202.0%20User%20Guide.pdf
 
 Here are the most used ones:
 **command, takeoff, land, up, down, left, right, forward, back, flip, speed, speed?, battery?**
--- index.js
+++ index.js
@@ -6,10 +6,18 @@ module.exports = {
      * Connects to the drone and returns a new Drone object
      * @param {Object} config - The optional config object
      * @param {String} config.host - The IP of the Tello drone
-     * @param {(String | Number)} config.port - The port of the UDP communication
-     * @param {(String | Number)} config.statePort - The port of the UDP state communication
+     * @param {String} config.port - The port of the UDP communication
+     * @param {String} config.statePort - The port of the UDP state communication
+     * @param {Number} config.bufferOffset - Offset in the buffer where the message starts
+     * @param {Boolean} config.skipOK - Dont send the ok message
+     * @param {Boolean} config.async - Make the send method async
      */
-    connect: config => new Drone(config)
+    connect: config => new Drone(config),
+
+    /**
+     * The delay timings in ms for each command
+     */
+    DELAYS: delays
 };
 
 /**
@@ -36,24 +44,41 @@ function parseDroneState(state) {
  * All options are optional
  * @param {Object} config - The optional config object
  * @param {String} config.host - The IP of the Tello drone
- * @param {(String | Number)} config.port - The port of the UDP communication
- * @param {(String | Number)} config.statePort - The port of the UDP state communication
+ * @param {String} config.port - The port of the UDP communication
+ * @param {String} config.statePort - The port of the UDP state communication
  * @param {Number} config.bufferOffset - Offset in the buffer where the message starts
+ * @param {Boolean} config.skipOK - Dont send the ok message
+ * @param {Boolean} config.async - Make the send method async
  */
-function Drone({ host = "192.168.10.1", port = "8889", statePort = "8890", bufferOffset = 0 } = {}) {
+function Drone({
+    host = "192.168.10.1",
+    port = "8889",
+    statePort = "8890",
+    bufferOffset = 0,
+    skipOK = false,
+    async = false
+} = {}) {
+    assertType("string", host);
+    assertType("string", port);
+    assertType("string", statePort);
+    assertType("number", bufferOffset);
+    assertType("boolean", skipOK);
+    assertType("boolean", async);
+
     this.HOST = host;
     this.MAIN_PORT = port;
     this.STATE_PORT = statePort;
     this.BUFFER_OFFSET = bufferOffset;
     this.droneIo = dgram.createSocket("udp4");
     this.droneState = dgram.createSocket("udp4");
+    this.connected = false;
 
     this.events = {
         connection: [],
         state: [],
         send: [],
-        error: [],
         message: [],
+        _ok: [],
 
         call: (event, ...args) => {
             if (!(event in this.events) || event == "call" || event == "attach") {
@@ -66,6 +91,8 @@ function Drone({ host = "192.168.10.1", port = "8889", statePort = "8890", buffe
             for (let callback of this.events[event]) {
                 callback(...args);
             }
+
+            if (event === "_ok") this.events._ok = [];
         },
 
         attach: (event, callback) => {
@@ -90,7 +117,7 @@ function Drone({ host = "192.168.10.1", port = "8889", statePort = "8890", buffe
     };
 
     /**
-     * Sends a command to the drone
+     * Sends a command to the drone asynchronously and waits a predefined amount of time before resolving
      * @param {"command" | "takeoff" | "land" | "streamon" | "streamoff" | "emergency" | "up" | "down" | "left" | "right" | "forward" | "back" | "cw" | "ccw" | "flip" | "go" | "curve" | "speed" | "rc" | "wifi" | "speed?" | "battery?" | "time?" | "wifi?" | "sdk?" | "sn?"} command
      * @param {Object} options - The option object
      * @param {Number} options.value - Number of cm the drone should move in a certain direction
@@ -103,29 +130,143 @@ function Drone({ host = "192.168.10.1", port = "8889", statePort = "8890", buffe
      * @param {Number} options.y1 - Starting y position for the curve command. Max: 500, Min: -500.
      * @param {Number} options.y2 - Ending y position for the curve command. Max: 500, Min: -500.
      */
-    this.send = (command, options) => {
-        if (options) {
-            verifyCommand(command, options);
+    this.send = (command, options) =>
+        async ? asyncSend.bind(this, command, options)() : syncSend.bind(this, command, options)();
 
-            for (let value of Object.values(options)) {
-                command += ` ${value}`;
-            }
-        }
-        this.droneIo.send(command, this.BUFFER_OFFSET, command.length, this.MAIN_PORT, this.HOST, this.events.call.bind(null, "send"));
-    };
+    this.forceSend = (command, options) =>
+        async
+            ? forceSendAsync.bind(this, command, options)()
+            : forceSendSync.bind(this, command, options)();
 
     this.droneIo.bind(this.MAIN_PORT);
     this.droneState.bind(this.STATE_PORT);
 
-    // this.droneIo.on("message", ...args => this.events.call("message", ...args));
-    // this.droneState.on("message", ...args => this.events.call("state", ...args));
-
-    this.droneIo.on("message", this.events.call.bind(null, "message"));
     this.droneState.on("message", this.events.call.bind(null, "state"));
-    this.droneState.on("error", this.events.call.bind(null, "state"));
-    this.droneState.on("listening", this.events.call.bind(null, "connection"));
 
-    this.send("command");
+    this.droneIo.on("message", (...args) => {
+        let message = args[0].toString();
+
+        if (message === "ok") {
+            this.events.call("_ok");
+
+            if (!this.connected) {
+                this.connected = true;
+                this.events.call("connection")
+            }
+
+            if (!skipOK) this.events.call("message", ...args);
+
+        } else {
+            this.events.call("message", ...args);
+        }
+    });
+
+    // this.send("command")
+}
+
+function forceSendSync(command) {
+    this.droneIo.send(
+        command,
+        this.BUFFER_OFFSET,
+        command.length,
+        this.MAIN_PORT,
+        this.HOST,
+        this.events.call.bind(null, "send")
+    );
+}
+
+function forceSendAsync(command) {
+    return new Promise((resolve, reject) => {
+        let _command = command;
+
+        this.droneIo.send(
+            Buffer.from(_command),
+            this.BUFFER_OFFSET,
+            _command.length,
+            this.MAIN_PORT,
+            this.HOST,
+            this.events.call.bind(null, "send")
+        );
+
+        // If the command is a read command resolve immediately otherwise wait for OK from drone
+        command.includes("?") ? resolve() : this.on("_ok", resolve);
+    });
+}
+
+/**
+ * Sends a command to the drone asynchronously and waits a predefined amount of time before resolving
+ * @param {"command" | "takeoff" | "land" | "streamon" | "streamoff" | "emergency" | "up" | "down" | "left" | "right" | "forward" | "back" | "cw" | "ccw" | "flip" | "go" | "curve" | "speed" | "rc" | "wifi" | "speed?" | "battery?" | "time?" | "wifi?" | "sdk?" | "sn?"} command
+ * @param {Object} options - The option object
+ * @param {Number} options.value - Number of cm the drone should move in a certain direction
+ * @param {Number} options.speed - How fast the drone should move. Max: 100, Min: 10.
+ * @param {Number} options.x - Number of cm the drone should move in the x axis. Max: 500, Min: -500.
+ * @param {Number} options.y - Number of cm the drone should move in the y axis. Max: 500, Min: -500.
+ * @param {Number} options.z - Number of cm the drone should move in the z axis. Max: 500, Min: -500.
+ * @param {Number} options.x1 - Starting x position for the curve command. Max: 500, Min: -500.
+ * @param {Number} options.x2 - Ending x position for the curve command. Max: 500, Min: -500.
+ * @param {Number} options.y1 - Starting y position for the curve command. Max: 500, Min: -500.
+ * @param {Number} options.y2 - Ending y position for the curve command. Max: 500, Min: -500.
+ */
+
+function asyncSend(command, options) {
+    return new Promise((resolve, reject) => {
+        let _command = command;
+
+        let error = verifyCommand(_command, options, true);
+
+        if (error) reject(error);
+
+        if (options) {
+            for (let value of Object.values(options)) {
+                _command += ` ${value}`;
+            }
+        }
+
+        this.droneIo.send(
+            Buffer.from(_command),
+            this.BUFFER_OFFSET,
+            _command.length,
+            this.MAIN_PORT,
+            this.HOST,
+            this.events.call.bind(null, "send")
+        );
+
+        // If the command is a read command resolve immediately otherwise wait for OK from drone
+        command.includes("?") ? resolve() : this.on("_ok", resolve);
+    });
+}
+
+/**
+ * Sends a command to the drone
+ * @param {"command" | "takeoff" | "land" | "streamon" | "streamoff" | "emergency" | "up" | "down" | "left" | "right" | "forward" | "back" | "cw" | "ccw" | "flip" | "go" | "curve" | "speed" | "rc" | "wifi" | "speed?" | "battery?" | "time?" | "wifi?" | "sdk?" | "sn?"} command
+ * @param {Object} options - The option object
+ * @param {Number} options.value - Number of cm the drone should move in a certain direction
+ * @param {Number} options.speed - How fast the drone should move. Max: 100, Min: 10.
+ * @param {Number} options.x - Number of cm the drone should move in the x axis. Max: 500, Min: -500.
+ * @param {Number} options.y - Number of cm the drone should move in the y axis. Max: 500, Min: -500.
+ * @param {Number} options.z - Number of cm the drone should move in the z axis. Max: 500, Min: -500.
+ * @param {Number} options.x1 - Starting x position for the curve command. Max: 500, Min: -500.
+ * @param {Number} options.x2 - Ending x position for the curve command. Max: 500, Min: -500.
+ * @param {Number} options.y1 - Starting y position for the curve command. Max: 500, Min: -500.
+ * @param {Number} options.y2 - Ending y position for the curve command. Max: 500, Min: -500.
+ */
+function syncSend(command, options) {
+    verifyCommand(command, options);
+
+    if (options) {
+        for (let value of Object.values(options)) {
+            command += ` ${value}`;
+        }
+    }
+
+    this.droneIo.send(
+        command,
+        this.BUFFER_OFFSET,
+        command.length,
+        this.MAIN_PORT,
+        this.HOST,
+        this.events.call.bind(null, "send")
+    );
 }
 
 /**
@@ -133,24 +274,34 @@ function Drone({ host = "192.168.10.1", port = "8889", statePort = "8890", buffe
  * @param {String} command The drone command
  * @param {Object} options Optional object that goes along the command
  */
-function verifyCommand(command, options) {
+function verifyCommand(command, options, returns = false) {
     assertType("string", command);
 
+    //Check if the command is valid
     if (!validCommands.all.includes(command)) {
         throw new Error(`Invalid command: ${command}`);
+    } else if (!options) {
+        //If the command is valid but expects options to go along with it
+        if (command in commandLimits) {
+            throw new Error(`Expected an option object along with the command: ${command}`);
+        }
+
+        return;
     }
 
     assertType("object", options);
 
+    //Check if there are any missing properties in the options object
     for (let prop in commandLimits[command]) {
         if (!(prop in options)) {
-            throw new Error(`Expected ${prop} in options but recieved undefined`);
+            throw new Error(`Expected '${prop}' in options but recieved undefined`);
         }
     }
 
+    //Check if there are any additional properties in the options object
     for (let prop in options) {
         if (!(prop in commandLimits[command])) {
-            throw new Error(`Unexpected ${prop} in options`);
+            throw new Error(`Unexpected '${prop}' in options`);
         }
     }
 
@@ -158,10 +309,17 @@ function verifyCommand(command, options) {
         let value = options[prop];
         let limit = commandLimits[command][prop];
 
+        //Check if the command has a limit on the value from the tello SDK
         if ("max" in limit && "min" in limit) {
             if (!(value <= limit.max && value >= limit.min)) {
-                throw new Error(`Invalid value ${prop}: ${value}, expected ${prop} to range between ${limit.min}, ${limit.max}`);
+                throw new Error(
+                    `Invalid value ${prop}: ${value}, expected ${prop} to range between ${
+                    limit.min
+                    }, ${limit.max}`
+                );
             }
+
+            //If the command needs a single value, check if that is passed (stored as an array in tello-data.json)
         } else if (Array.isArray(limit)) {
             if (!limit.includes(value)) {
                 throw new Error(`Invalid value ${prop}: ${value} expected ${limit}`);
@@ -181,7 +339,7 @@ function assertType(expected, actual) {
     const errString = `Expected ${expected}, recieved: ${actual}, ${typeof actual}`;
 
     if (typeof expected !== "string") {
-        throw new Error("AssertType funciton expects a string");
+        throw new Error("AssertType function expects a string as expected parameter");
     }
 
     if (expected === "array") {
