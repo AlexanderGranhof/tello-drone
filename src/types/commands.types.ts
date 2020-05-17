96734262a0c21b9b3333394ee19bf59256a5d851
Alexander Granhof <alexander.g737@gmail.com>
2020-05-17 17:38:52 +0200

    Created types for commands and its options

---
+++ src/types/commands.types.ts
@@ -0,0 +1,45 @@
+export type ValidCommands =
+    "takeoff" |
+    "land" |
+    "streamon" |
+    "streamoff" |
+    "emergency" |
+    "up" |
+    "down" |
+    "left" |
+    "right" |
+    "forward" |
+    "back" |
+    "cw" |
+    "ccw" |
+    "flip" |
+    "go" |
+    "curve" |
+    "speed" |
+    "rc" |
+    "wifi" |
+    "speed?" |
+    "battery?" |
+    "time?" |
+    "wifi?" |
+    "sdk?" |
+    "sn?"
+    ;
+
+export interface ValidCommandOptions {
+    value?: number | string,
+    x?: number,
+    y?: number,
+    z?: number,
+    x1?: number,
+    y1?: number,
+    z1?: number,
+    x2?: number,
+    y2?: number,
+    z2?: number,
+    speed?: number,
+    a?: number,
+    b?: number,
+    c?: number,
+    d?: number
+}
